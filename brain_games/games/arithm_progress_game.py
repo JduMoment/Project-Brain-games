c70691fae82b887310f8a66b4b7d86694e15209f
Kuragin Ivan <curagin.vanya@yandex.ru>
2023-10-16 08:40:43 +0300

    fix proggress game

--- brain_games/games/arithm_progress_game.py
+++ brain_games/games/arithm_progress_game.py
@@ -6,7 +6,7 @@ RULES = 'What number is missing in the progression?'
 
 
 def generate_question_and_answer():
-    arithm_progress = list(range(randint(1, 100), randint(1, 1000),
+    arithm_progress = list(range(randint(1, 95), randint(100, 1000),
                                  randint(2, 7)))
     arithm_progress = arithm_progress[0:randint(5, 15)]
     correct_answer = choice(arithm_progress)
